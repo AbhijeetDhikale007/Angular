8f6232a955255a9b63bac17a14aea1a261e41404
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2024-04-01 06:31:57 +0000

    Done

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+// import {} from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home'},
+  {path: 'Contact', component:FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
